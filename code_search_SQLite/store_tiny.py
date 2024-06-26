76ca305acc54bc372b697972a49a2ae209c5f44f
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-06-26 06:41:43 -0500

    Update store_tiny.py

--- code_search_SQLite/store_tiny.py
+++ code_search_SQLite/store_tiny.py
@@ -31,16 +31,7 @@ largest_category = '''
     GROUP BY 1 ORDER BY 2 DESC LIMIT 1;'''
 
 expensive = cursor.execute(expensive_items).fetchall()
-# [(9, 'Mishi Kobe Niku', 4, 6, '18 - 500 g pkgs.', 97, 29, 0, 0, 1)
-# (18, 'Carnarvon Tigers', 7, 8, '16 kg pkg.', 62.5, 42, 0, 0, 0)
-# (20, "Sir Rodney's Marmalade", 8, 3, '30 gift boxes', 81, 40, 0, 0, 0)
-# (28, 'R▒ssle Sauerkraut', 12, 7, '25 - 825 g cans', 45.6, 26, 0, 0, 1)
-# (29,'Th▒ringer Rostbratwurst',12, 6,'50 bags x 30 sausgs.',123.79, 0, 0, 0,1)
-# (38, 'C▒te de Blaye', 18, 1, '12 - 75 cl bottles', 263.5, 17, 0, 15, 0)
-# (43, 'Ipoh Coffee', 20, 1, '16 - 500 g tins', 46, 17, 10, 25, 0)
-# (51, 'Manjimup Dried Apples', 24, 7, '50 - 300 g pkgs.', 53, 20, 0, 10, 0)
-# (59, 'Raclette Courdavault', 28, 4, '5 kg pkg.', 55, 79, 0, 0, 0)
-# (62, 'Tarte au sucre', 29, 3, '48 pies', 49.3, 17, 0, 0, 0)]
+
 
 avg_age = cursor.execute(avg_hire_age).fetchall()
 # [(37.22222222222222,)]
