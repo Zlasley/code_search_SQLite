21630a556762c768fe873caa24af6055a9d20595
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-09-28 21:37:25 -0500

    Update practice_data.py

--- code_search_SQLite/practice_data.py
+++ code_search_SQLite/practice_data.py
@@ -1,5 +1,5 @@
 """ Module providing function for creatin a SQL DB"""
-import sqlite3z
+import sqlite3zz
 
 ''' Creates a connection to sqlite3 database'''
 connection = sqlite3.connect("practice_data.sqlite3")
